f732dcbd33e3c3fc188ea14208142509d0a68d7a
priskaaimee <likarsaaimee@gmail.com>
2025-05-02 18:28:23 +0700

    added flame sensor detection and led and lcd change

--- src/main.cpp
+++ src/main.cpp
@@ -41,10 +41,13 @@ int main() {
         // Flame detected, activate alarm
         alarmActive = 1;
         startBuzzer();
+        clearDisplay();
         moveCursor(0, 1); // moves the cursor to 1, position
         writeString("Flame Detected!");
         greenLEDOff();
 
+        moveCursor(0, 0); // moves the cursor to 0, position
+        writeString("Red ON");
         redLEDOn();
         delayMs(100);
         redLEDOff();
@@ -55,7 +58,7 @@ int main() {
         moveCursor(0, 1); // moves the cursor to 1, position
         writeString("No Flame Detected");
         greenLEDOn();
-        moveCursor(0, 0); // moves the cursor to 1, position
+        moveCursor(0, 0); // moves the cursor to 0, position
         writeString("Green ON");
     }
 

f5e15886b1273d0e8d3d2c33c7e2a59874eb5471
Jennifernwaa <Jennifernwachinaemere@gmail.com>
2025-05-05 15:54:52 +0700

    callibrate (?)

--- src/main.cpp
+++ src/main.cpp
@@ -43,29 +43,54 @@ int main() {
   initGasSensor();
 
   while (1) {
+
+    float sensor_volt;
+    float RS_air; //  Get the value of RS via in a clear air
+    float R0;  // Get the value of R0 via in LPG
+    float sensorValue;
+
+    /*--- Get a average data by testing 100 times ---*/
+    for(int x = 0 ; x < 100 ; x++)
+    {
+        sensorValue = sensorValue + analogRead(A0);
+    }
+    sensorValue = sensorValue/100.0;
+    /*-----------------------------------------------*/
+
+    sensor_volt = sensorValue/1024*5.0;
+    RS_air = (5.0-sensor_volt)/sensor_volt; // omit *RL
+    R0 = RS_air/9.9; // The ratio of RS/R0 is 9.9 in LPG gas from Graph (Found using WebPlotDigitizer)
+
+    Serial.print("sensor_volt = ");
+    Serial.print(sensor_volt);
+    Serial.println("V");
+
+    Serial.print("R0 = ");
+    Serial.println(R0);
+    delay(1000);
       // Process gas sensor data
-      processGasSensor();
+      // processGasSensor();
 
-      if (isFlameDetected()) {
-          alarmActive = 1;
-          redLEDOn();
-          greenLEDOff();
-          startBuzzer();
-          updateDisplay("ALARM!", "Flame Detected!");
-      } else if (isGasDetected()) {
-          alarmActive = 1;
-          redLEDOn();
-          greenLEDOff();
-          startBuzzer();
-          updateDisplay("ALARM!", "Gas Detected!");
-      } else {
-          alarmActive = 0;
-          redLEDOff();
-          greenLEDOn();
-          updateDisplay("No Threat", "Detected");
-      }
+      // if (isFlameDetected()) {
+      //     alarmActive = 1;
+      //     redLEDOn();
+      //     greenLEDOff();
+      //     startBuzzer();
+      //     updateDisplay("ALARM!", "Flame Detected!");
+      // } else if (isGasDetected()) {
+      //     alarmActive = 1;
+      //     redLEDOn();
+      //     greenLEDOff();
+      //     startBuzzer();
+      //     updateDisplay("ALARM!", "Gas Detected!");
+      // } else {
+      //     alarmActive = 0;
+      //     redLEDOff();
+      //     greenLEDOn();
+      //     updateDisplay("No Threat", "Detected");
+      // }
 
-      delay(1000); // Adjust delay as needed for monitoring frequency
+      // delay(1000); // Adjust delay as needed for monitoring frequency
   }
 
   return 0;
